d50ae18dbedb19c7d448686fcb9ca8c122be9907
qiyubrother <13240137763@163.com>
2019-08-08 08:00:06 +0800

    .

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppParallelDemo
 {
+    // ForEach并行计算
     class ProgramDemo1
     {
         static void Main(string[] args)
@@ -34,7 +35,8 @@ namespace ConsoleAppParallelDemo
         }
     }
 
-    class Program
+    // 并行计算，超过3秒抛出异常
+    class ProgramDemo2
     {
         static void Main(string[] args)
         {
@@ -57,6 +59,47 @@ namespace ConsoleAppParallelDemo
             catch(AggregateException ex)
             {
                 foreach(Exception innerEx in ex.InnerExceptions)
+                {
+                    Debug.WriteLine(innerEx.ToString());
+                }
+            }
+
+        }
+
+        class TestInputData
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+
+    // 并行计算，限制最大并行度
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parallelOptions = new ParallelOptions();
+            // Environment.ProcessorCount; // 逻辑内核的数量
+            parallelOptions.MaxDegreeOfParallelism = 6; // 最大并行度
+            var sw = Stopwatch.StartNew();
+            var inputData = new List<TestInputData>();
+            try
+            {
+                var loopResult = Parallel.ForEach<TestInputData>(inputData, parallelOptions, (data, loopStatus) => {
+                    if (loopStatus.ShouldExitCurrentIteration)
+                    {
+                        return;
+                    }
+
+                    if (sw.Elapsed.Seconds > 3)
+                    {
+                        throw new TimeoutException("Parallel.ForEach istaking more than 3 seconds to complate.");
+                    }
+                });
+            }
+            catch (AggregateException ex)
+            {
+                foreach (Exception innerEx in ex.InnerExceptions)
                 {
                     Debug.WriteLine(innerEx.ToString());
                 }
